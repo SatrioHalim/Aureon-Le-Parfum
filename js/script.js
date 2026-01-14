e2802726ea652a1f227ef369d12232e952b55b39
SatrioHalim <satriohalim21@gmail.com>
2026-01-14 21:21:26 +0700

    UI Search Features Completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Hamburger menu clicked
@@ -6,11 +6,29 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle("active");
 } 
 
-// Klik diluar hamburger menu untuk menghilangkan navbar
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector('#search-box'); 
+
+document.querySelector('#search-button').onclick = (e) => {
+    // toggle untuk menambahkan atau menghilangkan class active
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    // e untuk menghilangkan fungsi default dari sebuah elemen
+    // disini casenya agar ketika tombol search di klik tidak langsung menuju ke link # ato scroll keatas
+    e.preventDefault();
+}
+
+// Klik diluar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
 document.addEventListener('click', function(e){
     // selama yang di klik bukan hamburger dan bukan navbar
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
         navbarNav.classList.remove('active');
+    }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove('active');
     }
 })
