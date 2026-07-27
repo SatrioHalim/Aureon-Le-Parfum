3a3ceae69417705120b7370424e80604b1f7321b
SatrioHalim <satriohalim21@gmail.com>
2026-07-27 23:07:46 +0700

    Feat : shopping cart & detail modal UI

--- js/script.js
+++ js/script.js
@@ -2,33 +2,68 @@
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Hamburger menu clicked
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle("active");
-} 
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
 
 // Toggle class active search form
 const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector('#search-box'); 
+const searchBox = document.querySelector("#search-box");
 
-document.querySelector('#search-button').onclick = (e) => {
-    // toggle untuk menambahkan atau menghilangkan class active
-    searchForm.classList.toggle('active');
-    searchBox.focus();
-    // e untuk menghilangkan fungsi default dari sebuah elemen
-    // disini casenya agar ketika tombol search di klik tidak langsung menuju ke link # ato scroll keatas
-    e.preventDefault();
-}
+document.querySelector("#search-button").onclick = (e) => {
+  // toggle untuk menambahkan atau menghilangkan class active
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  // e untuk menghilangkan fungsi default dari sebuah elemen
+  // disini casenya agar ketika tombol search di klik tidak langsung menuju ke link # ato scroll keatas
+  e.preventDefault();
+};
+
+// Toggle class active shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
 // Klik diluar elemen
-const hm = document.querySelector('#hamburger-menu');
-const sb = document.querySelector('#search-button');
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
-document.addEventListener('click', function(e){
-    // selama yang di klik bukan hamburger dan bukan navbar
-    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
-    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
-        searchForm.classList.remove('active');
-    }
-})
+document.addEventListener("click", function (e) {
+  // selama yang di klik bukan hamburger dan bukan navbar
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Close Modal Button
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik diluar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
